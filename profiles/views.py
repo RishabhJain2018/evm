1017b05ea399ded77f3f2ce2e32f34db8c941e41
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-09-15 02:18:58 +0530

    Added User profile forms.

--- profiles/views.py
+++ profiles/views.py
@@ -4,27 +4,22 @@ from django.http import HttpResponse
 from braces.views import LoginRequiredMixin
 
 def bad_request_404(request):
-	response = render(request, '404.html')
-	response.status_code = 404
-	return response
+    response = render(request, '404.html')
+    response.status_code = 404
+    return response
 
 
 def bad_request_500(request):
-	response = render(request, '500.html')
-	response.status_code = 500
-	return response
+    response = render(request, '500.html')
+    response.status_code = 500
+    return response
 
 
 class Home(View):
-	'''
-	This is the home page view function.
-	'''
+    '''
+    This is the home page view function.
+    '''
 
-	def get(self, request):
-		template_name = 'profiles/base.html'
-		return render(request, template_name)
-
-
-
-
-
+    def get(self, request):
+        template_name = 'profiles/base.html'
+        return render(request, template_name)
